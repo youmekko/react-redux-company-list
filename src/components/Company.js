d59fe4feba3a3b5e1332b8ee600a52ca5caed505
youmekko <youmekko.o@gmail.com>
2020-08-26 19:38:24 +0900

    style : styled-component 적용

--- src/components/Company.js
+++ src/components/Company.js
@@ -2,22 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Card, Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+import { CardWrapper, CompanyLevel, CompanyTitle } from './styles/style'
 
 const { Meta } = Card
-
 function Company({ company }) {
     return (
         <>
-            <Card style={{ width: '100%', marginBottom: 15 }}>
+            <CardWrapper>
                 <Meta
                     avatar={
                         <Avatar size={64} icon={<UserOutlined />} />
                     }
                     title={
                         <>
-                        <span style={{ color: '#1672f7', fontWeight: 'bold' }}>
+                        <CompanyLevel>
                             {company.level}등급
-                        </span>
+                        </CompanyLevel>
                         <span>
                             &nbsp;{company.company}
                         </span>
@@ -25,12 +25,14 @@ function Company({ company }) {
                     }
                     description={
                         <>
-                            <p style={{ color: '#000' }}>{company.title}</p>
+                            <CompanyTitle>
+                                {company.title}
+                            </CompanyTitle>
                             {company.is_full && <p>마감 되었습니다.</p>}
                         </>
                     }
                 />
-            </Card>
+            </CardWrapper>
         </>
     )
 }
---
+++ src/components/styles/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { Card } from 'antd'
+
+export const CardWrapper = styled(Card)`
+    width: 100%;
+    margin-bottom: 15px;
+`
+
+export const CompanyLevel = styled.span`
+    color: #1672f7;
+    font-weight: bold
+`
+export const CompanyTitle = styled.p`
+    color: #000
+`
